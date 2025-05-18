333cff621850abf0498e4abf10191243d6ded438
gongireddysr <gongireddysr@gmail.com>
2025-05-17 21:15:10 -0400

    Made few changes in Home Section

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -12,9 +12,9 @@ function Navbar() {
             </div>
             <div className="nav-menu">
                 <ul className="nav-links">
-                    <li><a href="#home"> Home </a></li>
-                    <li><a href="#about"> About </a></li>
-                    <li><a href="/SaiKrishna__Resume.pdf" target="_blank" rel="noopener noreferrer"> Resume </a></li>
+                    <li><a className = "nav-link" href="#home"> Home </a></li>
+                    <li><a className = "nav-link" href="#about"> About </a></li>
+                    <li><a className = "nav-link" href="/SaiKrishna__Resume.pdf" target="_blank" rel="noopener noreferrer"> Resume </a></li>
                 </ul>
             </div>
         </nav>
--- my-portfolio/src/sections/Home.tsx
+++ my-portfolio/src/sections/Home.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
-function Home() {
-  return <div>Home Section</div>;
-}
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="intro-title">LET ME INTRODUCE MYSELF</h1>
+        
+        <p className="intro-text">
+          I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️
+        </p>
+
+        <div className="skills-list">
+          I am fluent in classics like HTML, CSS, Javascript and React.
+        </div>
+
+        <p className="interests">
+          My field of Interest's are building new Web Technologies and Products
+        </p>
+
+        <p className="intro-text">
+          Whenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks like React.js and Next.js.
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
