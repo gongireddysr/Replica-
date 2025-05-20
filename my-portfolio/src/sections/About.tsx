5f8411a886ffa78686565118810057786d578764
gongireddysr <gongireddysr@gmail.com>
2025-05-19 22:56:34 -0400

    Started About Section

--- my-portfolio/src/sections/About.tsx
+++ my-portfolio/src/sections/About.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 function About() {
-  return <div>About Section</div>;
+  return <div>
+    <h1>About</h1>
+    <p>I am a software engineer with a passion for building scalable and efficient systems.</p>
+  </div>;
 }
 export default About;
