11fb029aa518b7ede887f64503ffc230f814fad4
gongireddysr <gongireddysr@gmail.com>
2025-07-04 13:10:21 -0400

    updated About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import {Col, Row, Container} from 'react-bootstrap';
+import Particle from '../Particle';
+import Github from './Github'
+import LaptopImg from '../Assets/about.png'
+
 
 const About: React.FC = () => {
   return (
-    <section id="about" className="about-section">
-      <div className="about-content">
-        <h2>About Me</h2>
-        <p>I'm a dedicated developer with a passion for creating innovative solutions.</p>
-      </div>
-    </section>
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{justifyContent:"center", padding:"10px"}}>
+          <Col md={7} style={{justifyContent:"center", paddingTop:"30px", paddingBottom:"50px"}}>
+            <h1 style={{fontSize: "2.1em", paddingBottom: "20px"}}>
+              Know Who <strong className="purple">I'M</strong>
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 };
 
