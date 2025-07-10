1d8d3b3f4b9f60e6c9e8fbef18c0f55ca16446cd
gongireddysr <gongireddysr@gmail.com>
2025-07-09 22:20:21 -0400

    updated Aboutcard section

--- src/components/About/AboutCards.tsx
+++ src/components/About/AboutCards.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Col, Row} from "react-bootstrap";
+import { Col, Row, Card} from "react-bootstrap";
 
 const Aboutcards: React.FC = () => {
     return(
-        <Row>
-
-        </Row>
+        <Card className="quote-card-view">
+            <Card.Body>
+            <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sai Krishna </span>
+            from <span className="purple"> Hyderabad, India.</span>
+            <br />
+            I am currently a Student at Indiana Weselyean University.
+            <br />
+            I am doing my Master's (MS) in Information Technology.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+            </Card.Body>
+        </Card>
     );
 };
 export default Aboutcards;
